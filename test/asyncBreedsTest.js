51d79dd2295fabae7e0061b8f27ec50ea68f9e58
Jeffery <parkjon8@gmail.com>
2021-12-01 18:45:59 +0000

    Added test/ayncBreedsTest.js and test/syncBreedsTest.js

---
+++ test/asyncBreedsTest.js
@@ -0,0 +1,33 @@
+const assert = require('chai').assert;
+const breedDetailsFromFile = require('../asyncBreeds');
+
+describe('#breedDetailsFromFile', () => {
+  it('provides, via callback, breed details for the Bombay breed', (done) => {
+    breedDetailsFromFile('Bombay', (bombay) => {
+      const expectedDescription = "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.";
+      assert.equal(expectedDescription, bombay);
+      done();
+    });
+  });
+
+  it('provides, via callback, breed details for the Balinese breed', (done) => {
+    breedDetailsFromFile('Balinese', (balinese) => {
+      const expectedDescription = "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.";
+      assert.equal(expectedDescription, balinese);
+      done();
+    });
+  });
+
+  it('provides, via callback, undefined for a breed that does not exist', (done) => {
+    breedDetailsFromFile('Saphire', (desc) => {
+      assert.equal(undefined, desc);
+      done(); 
+    });      
+  });
+});
+
+
+// const printBreedDetails = (breed => console.log('Return Value:', breed));
+
+// // we try to get the return value
+// const bombay = breedDetailsFromFile('Bombay', printBreedDetails);
